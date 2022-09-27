0a3f3ada27ae00f2b4c9938494ed67104f804a3b
Vinícius <viniscrv@gmail.com>
2022-09-27 00:03:57 -0300

    props Categoria

--- src/components/Produtos/Categoria/Categoria.tsx
+++ src/components/Produtos/Categoria/Categoria.tsx
@@ -1,51 +1,46 @@
 import { Container } from "./style";
-import produto01 from '../../../assets/img/produtosImg/unsplash_1VV1MRafd7A.png' 
-import produto02 from '../../../assets/img/produtosImg/unsplash_4OHkK555s1A.png' 
-import produto03 from '../../../assets/img/produtosImg/unsplash_6FDXGY9J6y4.png' 
-import produto04 from '../../../assets/img/produtosImg/unsplash_epRFE_hBNjo.png' 
-import produto05 from '../../../assets/img/produtosImg/unsplash_KeGToDVN0l4.png' 
-import produto06 from '../../../assets/img/produtosImg/unsplash_R8L1l9RN198.png' 
 
-export function Categoria(){
+
+export function Categoria(props: {categoria:string, produto1: string, produto2: string, produto3: string, produto4: string, produto5: string, produto6: string}){
     return (
         <Container>
                 <header>
-                    <h2>Star Wars</h2>
+                    <h2>{props.categoria}</h2>
                     <a href="">Ver tudo</a>
                 </header>
                 <div className="produtos">
                     <div>
-                        <img src={produto01} alt="" />
+                        <img src={props.produto1} alt="" />
                         <p className="titulo">Produto XYZ</p>
                         <p className="preco">R$ 60,00</p>
                         <a href="#">Ver produto</a>
                     </div>
                     <div>
-                        <img src={produto02} alt="" />
+                        <img src={props.produto2} alt="" />
                         <p className="titulo">Produto XYZ</p>
                         <p className="preco">R$ 60,00</p>
                         <a href="#">Ver produto</a>
                     </div>
                     <div>
-                        <img src={produto03} alt="" />
+                        <img src={props.produto3} alt="" />
                         <p className="titulo">Produto XYZ</p>
                         <p className="preco">R$ 60,00</p>
                         <a href="#">Ver produto</a>
                     </div>
                     <div>
-                        <img src={produto04} alt="" />
+                        <img src={props.produto4} alt="" />
                         <p className="titulo">Produto XYZ</p>
                         <p className="preco">R$ 60,00</p>
                         <a href="#">Ver produto</a>
                     </div>
                     <div>
-                        <img src={produto05} alt="" />
+                        <img src={props.produto5} alt="" />
                         <p className="titulo">Produto XYZ</p>
                         <p className="preco">R$ 60,00</p>
                         <a href="#">Ver produto</a>
                     </div>
                     <div>
-                        <img src={produto06} alt="" />
+                        <img src={props.produto6} alt="" />
                         <p className="titulo">Produto XYZ</p>
                         <p className="preco">R$ 60,00</p>
                         <a href="#">Ver produto</a>
--- src/components/Produtos/produtos.tsx
+++ src/components/Produtos/produtos.tsx
@@ -1,12 +1,55 @@
 import { Container } from "./style";
 import { Categoria } from "./Categoria/Categoria";
+import starWars1 from '../../assets/img/produtosImgStarWars/unsplash_1VV1MRafd7A.png' 
+import starWars2 from '../../assets/img/produtosImgStarWars/unsplash_4OHkK555s1A.png' 
+import starWars3 from '../../assets/img/produtosImgStarWars/unsplash_6FDXGY9J6y4.png' 
+import starWars4 from '../../assets/img/produtosImgStarWars/unsplash_epRFE_hBNjo.png' 
+import starWars5 from '../../assets/img/produtosImgStarWars/unsplash_KeGToDVN0l4.png' 
+import starWars6 from '../../assets/img/produtosImgStarWars/unsplash_R8L1l9RN198.png' 
+import console1 from '../../assets/img/produtosImgConsoles/unsplash_0POwK6iAiRQ.png'
+import console2 from '../../assets/img/produtosImgConsoles/unsplash_caNzzoxls8Q.png'
+import console3 from '../../assets/img/produtosImgConsoles/unsplash_k-xYhI3-gJM.png'
+import console4 from '../../assets/img/produtosImgConsoles/unsplash_wa5z9o9fgjw.png'
+import console5 from '../../assets/img/produtosImgConsoles/unsplash_Zjn4dT993-g.png'
+import console6 from '../../assets/img/produtosImgConsoles/unsplash_ZV7lnfyQLmA.png'
+import diversos1 from '../../assets/img/produtosImgDiversos/unsplash_bUgaIaZysH0.png'
+import diversos2 from '../../assets/img/produtosImgDiversos/unsplash_fMP-oCze3AY.png'
+import diversos3 from '../../assets/img/produtosImgDiversos/unsplash_jMT6BrgBuXU.png'
+import diversos4 from '../../assets/img/produtosImgDiversos/unsplash_MxVkWPiJALs.png'
+import diversos5 from '../../assets/img/produtosImgDiversos/unsplash_r27umXAelDc.png'
+import diversos6 from '../../assets/img/produtosImgDiversos/unsplash_sYVY_ZKwaxU.png'
 
 export function Produtos(){
     return (
         <Container>
-            <Categoria/>
-            <Categoria/>
-            <Categoria/>
+            <Categoria
+                categoria='Star Wars'
+                produto1={starWars1}
+                produto2={starWars2}
+                produto3={starWars3}
+                produto4={starWars4}
+                produto5={starWars5}
+                produto6={starWars6}
+            />
+            <Categoria
+                categoria='Consoles'
+                produto1={console1}
+                produto2={console2}
+                produto3={console3}
+                produto4={console4}
+                produto5={console5}
+                produto6={console6}
+            />
+            <Categoria
+                categoria='Diversos'
+                produto1={diversos1}
+                produto2={diversos2}
+                produto3={diversos3}
+                produto4={diversos4}
+                produto5={diversos5}
+                produto6={diversos6}
+            />
+            
         </Container>
     )
 }
