af874fee515d156b793578aa3669bf3e3bb0a9ed
Vinícius <viniscrv@gmail.com>
2022-10-05 23:43:42 -0300

    add page 'IdProduto'

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
+import IdProduto from './pages/IdProduto/IdProduto';
 import Login from './pages/Login';
 import NovoProduto from './pages/NovoProduto';
 import Produtos from './pages/Produtos';
@@ -12,6 +13,7 @@ function App() {
         <Route path="/Login" element={<Login/>}/>
         <Route path="/Produtos" element={<Produtos/>}/>
         <Route path="/NovoProduto" element={<NovoProduto/>}/>
+        <Route path="/IdProduto" element={<IdProduto/>}/>
       </Routes>
     </Router>
   )
---
+++ src/pages/IdProduto/IdProduto.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../../components/Header/Header";
+import { Rodape } from "../../components/Rodape/Rodape";
+import GlobalStyle from '../../styles/global';
+
+function IdProduto() {
+    return (
+      <>
+        <Header/>
+        <Rodape/>
+        <GlobalStyle />
+      </>
+    )
+  }
+  
+  export default IdProduto
