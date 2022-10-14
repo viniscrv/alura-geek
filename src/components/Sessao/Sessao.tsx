56c1d19b51dbdd6e7d93ade2dd5d23453adb819d
Vinícius <viniscrv@gmail.com>
2022-10-13 23:29:46 -0300

    update validacao de email

--- src/components/Sessao/Sessao.tsx
+++ src/components/Sessao/Sessao.tsx
@@ -1,20 +1,23 @@
 import { Container } from "./style";
-import { useForm } from "react-hook-form";
+import { useForm  } from "react-hook-form";
 
 
 export function Sessao(){
 
-    const { register, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm();
     const onSubmit = (d :any) => alert(JSON.stringify(d))
-
+    
     return (
         <Container>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <label htmlFor="email">Iniciar Sessão</label>
-                <input placeholder="Escreva seu email" id='email' {...register('email', { pattern: /^[a-z0-9.]+@[a-z0-9]+\.[a-z]/i })} />
-                <input type="password" placeholder="Escreva sua senha" />
+                {errors.email?  <p style={{color: 'red', marginBottom: '1em'}}>Formato de email inválido.</p>  : '' }
+                <input placeholder="Escreva seu email" id='email' {...register('email', { required: true, pattern: /^[a-z0-9.]+@[a-z0-9]+\.[a-z]/i })}
+                />
+                <input type="password" placeholder="Escreva sua senha" {...register('senha', {required: true})}/>
                 <input type="submit" value={'Enviar'}/>
             </form>
         </Container>
     )
 }
+
